902b089767b67ea7df894232113a4e340e0fae25
Dun-Njuguna <duncankariuki60@gmail.com>
2019-01-18 14:57:01 +0300

    created pipe

---
+++ src/app/showdate.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ShowdatePipe } from './showdate.pipe';
+
+describe('ShowdatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ShowdatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
