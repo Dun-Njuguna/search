d1e9f9c9fdbe9bb122372ad04c0fece094795289
Dun-Njuguna <duncankariuki60@gmail.com>
2019-01-21 17:57:32 +0300

    added not 404 page(routing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,28 @@ import { AppComponent } from './app.component';
 import { ShowdatePipe } from './showdate.pipe';
 import { HighlightDirective } from './highlight.directive';
 import { GetComponent } from './get/get.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
+const routes:Routes=[
+{path:'**',component:NotFoundComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     ShowdatePipe,
     HighlightDirective,
-    GetComponent
+    GetComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
